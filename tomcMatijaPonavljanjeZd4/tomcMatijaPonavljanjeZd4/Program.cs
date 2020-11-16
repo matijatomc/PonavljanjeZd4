c43c4d8e7c969484b2d1c78e5c8c7570942cb527
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-11-16 22:37:08 +0100

    Add files via upload

---
+++ tomcMatijaPonavljanjeZd4/tomcMatijaPonavljanjeZd4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPonavljanjeZd4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite jednu cijenu bez PDV-a: ");
+            decimal bez = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Unesite istu cijenu sa PDV-eom: ");
+            decimal sa = Convert.ToDecimal(Console.ReadLine());
+
+            decimal pdv = ((sa / bez)-1)*100;
+
+            Console.WriteLine("PDV je: " + pdv + "%");
+            Console.ReadKey();
+        }
+    }
+}
